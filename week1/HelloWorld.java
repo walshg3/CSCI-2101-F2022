97460528d9f461c7cf6cc6d01bda5128feea8677
Gregory J-ctr Walsh <gregory.j-ctr.walsh@faa.gov>
2022-09-22 18:11:18 -0400

    added some sysos

--- week1/HelloWorld.java
+++ week1/HelloWorld.java
@@ -3,5 +3,31 @@ package week1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("I like the Jets!"); // this is a string
+        System.out.println("3");
+        System.out.println('a');
+
+        System.out.println("Today is Thursday");
+
+
+
+        System.out.println('3');
+        // System.out.println(3+3); //Integer
+        int answer = 3+3;
+        System.out.println(answer);
+        String name = "Greg";
+        System.out.println(name + " is a great teacher! 6GOD X420");
+        // String Concatenation
+        System.out.println();
+        System.out.println(answer + name);
+        System.out.print("Hello\n");
+        System.out.println("I like eggs");
+
+        name = "Sam";
+
+        System.out.println(name);
+
+
+        
     }
 }
